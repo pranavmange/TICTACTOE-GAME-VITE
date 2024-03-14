92015f465f1ea6f6837d3c56943aaa21c4f8cf86
Pranav Mange <pranavmange2015@gmail.com>
2024-03-14 23:32:28 +0530

    no unsed var

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './component/Styles.scss';
 import Board from './component/Board';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const onBtnClick = () => {
+    console.log('hello');
+    setCounter(currentCounter => {
+      return currentCounter + 1;
+    });
+  };
+
   return (
     <div className="app">
-      <Board />
+      <div>
+        <button onClick={onBtnClick}>Click me Please</button>
+        <div>{counter}</div>
+      </div>
     </div>
   );
 }
